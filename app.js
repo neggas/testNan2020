790b869511767845f6ea1006c8453955f86adcac
neggas <arsene.fop@gmail.com>
2020-06-26 13:53:19 +0200

    ajout du module flash

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require('body-parser');
-
+const flash = require('connect-flash');
 const app = express();
 
 const http = require("http").createServer(app);
@@ -10,7 +10,7 @@ const contactes = require("./models/db");
 app.set('view engine', 'ejs'); //choix du moteur de template
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(flash())
 
 app.get('/login', (req, res, next) => {
     res.render('login');
@@ -23,12 +23,12 @@ app.post('/login', (req, res) => {
         res.redirect('/login');
     } else {
 
-        res.render('index', { contactes: contactes });
+        res.redirect('chat');
     }
 })
 
 app.get('/chat', (req, res, next) => {
-    res.render('index');
+    res.render('index', { contactes: contactes });
 });
 
 
